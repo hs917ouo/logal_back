3b78a7d38f26ab297e86bee43fbc0550f5461980
hs917ouo <hs917ouo@naver.com>
2025-01-10 10:30:37 +0900

    스프링 부트 세팅

---
+++ logal/src/main/java/com/hs917ouo/logal/LogalApplication.java
@@ -0,0 +1,13 @@
+package com.hs917ouo.logal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogalApplication.class, args);
+	}
+
+}
---
+++ logal/src/test/java/com/hs917ouo/logal/LogalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs917ouo.logal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
